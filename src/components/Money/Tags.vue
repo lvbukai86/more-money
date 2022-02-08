eeaf03e77ff36f36dc6e62126bba168d7391f835
ada <304368809@qq.com>
2022-02-08 11:53:46 +0800

    æ收集4个组件数据

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -18,7 +18,7 @@ import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 @Component
 export default class Tags extends Vue{
-  @Prop(Boolean) dataSource: string[] | undefined;
+  @Prop(Array) dataSource: string[] | undefined;
   selectedTags: string[]=[];
   toggle(tag: string){
     const index=this.selectedTags.indexOf(tag);
@@ -27,6 +27,7 @@ export default class Tags extends Vue{
     }else{
       this.selectedTags.push(tag);
     }
+    this.$emit('update:value',this.selectedTags);
   }
   create(){
     const name=window.prompt('请输入标签名');
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,33 +1,54 @@
 <template>
   <div>
     <layout class-prefix="layout">
-      <NumberPad/>
-      <Notes/>
-      <Types/>
-      <Tags :data-source.sync="tags"/>
-
+      {{record}}}
+      <NumberPad :value.sync="record.amount"/>
+      <Notes @update:value="onUpdateNotes"/>
+      <Types :value.sync="record.type"/>
+      <Tags :data-source.sync="tags" @update:value="OnUpdateTags"/>
     </layout>
   </div>
-
 </template>
-
-<script >
+<script lang="ts">
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
-export default {
-  name: 'Money',
-  components: {Tags, Types, Notes, NumberPad},
-  data(){
-    return{
-      tags:['衣','食','住','行']
-    }
+import {Component} from 'vue-property-decorator';
+import Vue from 'vue';
+type Record={
+  tags:string[]
+  notes:string
+  type:string
+  amount:number
+}
+
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+export default class Money extends Vue {
+
+  tags = ['衣', '食', '住', '行'];
+  record: Record ={
+    tags:[], notes:'',type:'-',amount:0
+  };
+
+  OnUpdateTags(value: string[]) {
+    this.record.tags=value;
   }
-};
+  onUpdateNotes(value:string){
+   this.record.notes=value;
+  }
+  onUpdateType(value:string){
+   this.record.type=value;
+  }
+  onUpdateAmount(value:string){
+   this.record.amount=parseFloat(value);
+  }
+}
 </script>
 <style lang="scss">
-.layout-content{
+.layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
