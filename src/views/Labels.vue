373b4dfb82742d26d305d38ae74bac74107fe3fc
ada <304368809@qq.com>
2022-02-10 20:50:49 +0800

    用挂windoæw来挂全局数据

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -28,7 +28,7 @@ import Button from '@/components/Button.vue';
   components: {Button}
 })
 export default class Labels extends Vue{
-  tags=tagListModel.fetch();
+  tags=window.tagList;
   createTag(){
     const name=window.prompt('请输入标签名');
     if(name){
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,15 +22,12 @@ import recordListModel from '@/models/recordListModel';
 import tagListModel from '@/models/tagListModel';
 
 const recordList=recordListModel.fetch();//获取记录
-const tagList=tagListModel.fetch();//获取标签
-
-
 @Component({
   components: {Tags,  FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
 
-  tags = tagList;
+  tags = window.tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem ={
     tags:[], notes:'',type:'-',amount:0
